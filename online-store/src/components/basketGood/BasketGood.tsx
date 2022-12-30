9ffc434402d45a919011d2cdb3ae7d1cdaf4f835
Mornival <ATAMAH_XAH@mail.ru>
2022-12-30 23:43:02 +0300

    feat: add transition to description page, add delete and add goods

--- online-store/src/components/basketGood/BasketGood.tsx
+++ online-store/src/components/basketGood/BasketGood.tsx
@@ -1,24 +1,66 @@
 import { IProduct } from "../../types/types";
-import React, { useState , useContext} from 'react';
+import React, { useState , useContext, useEffect} from 'react';
 import { DescriptionContext } from "../context/OtherContexts";
-import './BasketGood.scss'
+import './BasketGood.scss';
+import contextCart from "../context/contextCart";
+import { ICard } from "../../types/types";
+import { Link } from 'react-router-dom';
+import  contextProducts from '../context/contextProducts'
 interface PropsProduct{
-    product: IProduct
+    product: IProduct,
+    productId: number,
+    id: number,
+    number: number
 }
 
 function BasketGood(props: PropsProduct){
+    const [dataId, setDataId] = useState(0);
     let { setDescrition } = useContext(DescriptionContext);
+    const { dataCart, setDataCart} = useContext(contextCart);
+    const selectedProducts: IProduct[] = dataCart.map(item => item.objProduct);
     const clickImage = (e: React.MouseEvent<HTMLDivElement>) => {
         e.preventDefault();
-        console.log(e);
+        console.log(dataId);
         if(setDescrition) setDescrition();
+        console.log()
     }
-    const [numberOfGoods, setNumberOfGoods] = useState(1);
+    const addInCart = function(){
+        const arr = [...dataCart];
+        let numberOfGood: number = 0;
+        for(let i: number = 0; i < arr.length; i++){
+            if(dataId === arr[i].objProduct.id){
+                numberOfGood = i;
+                break;
+            }
+        }
+        arr.push(arr[numberOfGood]);
+        setDataCart(arr);
+    }
+    useEffect(function(){
+        setDataId(props.product.id);
+    },[dataCart]);
+    const minusInCart = function(){
+        const arr = [...dataCart];
+        let numberOfDeleted: number = 0;
+        let b: ICard[] = [];
+        for(let i = arr.length - 1; i >= 0; i--){
+            if(dataId === arr[i].objProduct.id){
+                numberOfDeleted = i;
+                break;
+            }
+        }
+        setDataCart(arr.filter((product , index)=>{
+            if(index === numberOfDeleted){
+                return false;
+            }
+            return true;
+        }));
+    }
     return(
         <div className = "good-body">
-            <div className="good-id">{props.product.id}</div>
+            <div className="good-id">{props.id + 1}</div>
             <div className = "good-image" onClick={clickImage}>
-                <img src = {props.product.images[0]} alt = "good"/>
+                <Link to = {`/details/${props.productId}`}><img src = {props.product.thumbnail} alt = "good"/></Link>
             </div>
             <div className = "good-info">
                 <h2>{props.product.title}</h2>
@@ -31,11 +73,11 @@ function BasketGood(props: PropsProduct){
             <div className = "good-other-info">
                 <h2 className = "good-other-info__h2">Stock: {props.product.stock}</h2>
                 <div className="good-amount">
-                    <div className = "good-amount__plus" onClick={function(){if(numberOfGoods !== props.product.stock) setNumberOfGoods(numberOfGoods + 1)}}>+</div>
-                    <h2 className = "good-amount__amount">{numberOfGoods}</h2>
-                    <div className = "good-amount__minus" onClick={function(){if(numberOfGoods > 1) setNumberOfGoods(numberOfGoods - 1)}} >-</div>
+                    <div className = "good-amount__plus" onClick={addInCart}>+</div>
+                    <h2 className = "good-amount__amount">{props.number}</h2>
+                    <div className = "good-amount__minus" onClick={minusInCart} >-</div>
                 </div>
-                <h2 className = "money-price">€{props.product.price * numberOfGoods}</h2>
+                <h2 className = "money-price">€{props.product.price * props.number}</h2>
             </div>
         </div>
     )
--- online-store/src/components/productDescriptionPage/ProductDescriptionPage.tsx
+++ online-store/src/components/productDescriptionPage/ProductDescriptionPage.tsx
@@ -1,77 +1,85 @@
 import './ProductDescriptionPage.scss';
 import { IProduct } from '../../types/types';
-import React, { useState , useContext} from 'react';
+import React, { useState , useContext } from 'react';
 import ModalContext,{DescriptionContext} from '../context/OtherContexts';
+import { Link , useParams } from 'react-router-dom';
 interface PropsDescription{
-    product: IProduct;
+    products: IProduct[];
 }
-function ProductDescription(props: PropsDescription){
-    const [image, setImage] = useState(0);
-    let {modal, setModal} = useContext(ModalContext);
-    let {open, setDescrition} = useContext(DescriptionContext);
-    const clickBuy = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        console.log(e);
-        if(open && setDescrition && setModal){
-            setDescrition();
-            setModal();
+function ProductDescription(propss: PropsDescription){
+    const id = useParams();
+    let b: number = 0;
+    let props: IProduct = propss.products[0];
+    console.log(id.id);
+    if(id.id){
+         b = (+id.id);
+    }
+    for(let i: number = 0; i < propss.products.length; i++){
+        // console.log(propss.products[i].id);
+        // console.log(b);
+        if(b === propss.products[i].id){
+            props = propss.products[i];
+            break;
         }
     }
-    const clickImage = (e: React.MouseEvent<HTMLImageElement>) => {
+    console.log(props);
+    // const [image, setImage] = useState(0);
+    let {modal, setModal} = useContext(ModalContext);
+    let {open, setDescrition} = useContext(DescriptionContext);
+    const clickBuy = (e: React.MouseEvent) => {
         e.preventDefault();
         console.log(e);
-        if(open && setDescrition && setModal){
-            setDescrition();
+        if(setModal && !modal){
             setModal();
         }
     }
     return(
     <div className="description-container">
         <div className="description-top">
-            <p className="description-top-p">store</p>
+            <p className="description-top-p"><Link to="/">store</Link></p>
             <p className="description-top-p">&gt;&gt;&gt;</p>
-            <p className="description-top-p">{props.product.category}</p>
+            <p className="description-top-p">{props.category}</p>
             <p className="description-top-p">&gt;&gt;&gt;</p>
-            <p className="description-top-p">{props.product.brand}</p>
+            <p className="description-top-p">{props.brand}</p>
             <p className="description-top-p">&gt;&gt;&gt;</p>
-            <p className="description-top-p">{props.product.title}</p>
+            <p className="description-top-p">{props.title}</p>
         </div>
-        <h2 className="description-name">{props.product.title}</h2>
+        <h2 className="description-name">{props.title}</h2>
         <div className="description-body">
             <div className="description-images">
-                {props.product.images.map((v,i) => <img className="description-images__image"src={v} key={i}/>)}
+                {props.images.map((v,i) => <img className="description-images__image"src={v} key={i}/>)}
             </div>
-            <div className ="description-main-image"><img src={props.product.images[image]}/></div>
+            <div className ="description-main-image"><img src={props.thumbnail}/></div>
             <div className="description-data">
                 <div className="description-data__item">
                     <h2>Description</h2>
-                    <p>{props.product.description}</p>
+                    <p>{props.description}</p>
                 </div>
                 <div className="description-data__item">
                     <h2>Discount Percentage</h2>
-                    <p>{props.product.discountPercentage}</p>
+                    <p>{props.discountPercentage}</p>
                 </div>
                 <div className="description-data__item">
                     <h2>Rating</h2>
-                    <p>{props.product.rating}</p>
+                    <p>{props.rating}</p>
                 </div>
                 <div className="description-data__item">
                     <h2>Stock</h2>
-                    <p>{props.product.stock}</p>
+                    <p>{props.stock}</p>
                 </div>
                 <div className="description-data__item">
                     <h2>Brand</h2>
-                    <p>{props.product.brand}</p>
+                    <p>{props.brand}</p>
                 </div>
                 <div className="description-data__item">
                     <h2>Category</h2>
-                    <p>{props.product.category}</p>
+                    <p>{props.category}</p>
                 </div>
             </div>
             <form className="description-buy">
-                <h2>€{props.product.price}</h2>
+                <h2>€{props.price}</h2>
                 <button className="description-button" type="button">DROP FROM CART</button>
-                <button className="description-button" type="button" onClick={clickBuy}>BUY NOW</button>
+                <button className="description-button" type="button"  onClick={clickBuy}><Link to="/basket">BUY NOW</Link></button>
             </form>
         </div>
     </div>
