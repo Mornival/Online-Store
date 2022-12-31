526e4befee7a91be9f00a8e3c75e6e7fcbf72350
Zheka <grushevskiy.yevgeniy@mail.ru>
2022-12-31 17:48:57 +0300

    try fix bag with footer

--- online-store/src/components/mainFooter/mainFooter.tsx
+++ online-store/src/components/mainFooter/mainFooter.tsx
@@ -1,8 +1,34 @@
 import './mainFooter.scss';
+import { useEffect, useContext , useState } from 'react';
+import ContextCart from '../context/contextCart';
+
+interface IDataStyle  {
+    [index:string]:string
+}
 
 const MainFooter = () => {
+    const url = window.location.pathname;
+    const [dataStyle, setDataStyle] = useState<IDataStyle>({});
+    const {dataCart} = useContext(ContextCart);
+    function changeDataStyle():void {
+        const HREF = window.location.href;
+        const goods = document.querySelectorAll(".good-body");
+        if(HREF.includes('basket') && goods.length <= 6) {
+            setDataStyle({
+                'position':'absolute',
+                'width': '100%',
+                'bottom':'0'
+            });
+        } else {
+            setDataStyle({});
+        }
+    }
+    useEffect(()=>{
+        changeDataStyle();
+    },[dataCart,url]);
+    
     return (
-        <footer className='footer'>
+        <footer style={dataStyle} className='footer'>
             Online Store 2022 Grushevskiy & Eroshenko &copy;
         </footer>
     );
