1bf4668f8cc7089c71be2c3d0f560456a8342c68
Mornival <ATAMAH_XAH@mail.ru>
2023-01-03 19:25:21 +0300

    feat: add promocods checking

--- online-store/src/components/basketSummary/BasketSummary.tsx
+++ online-store/src/components/basketSummary/BasketSummary.tsx
@@ -1,5 +1,5 @@
 import './BasketSummary.scss'
-import { useContext } from 'react';
+import { useContext , useState} from 'react';
 import { IProduct } from '../../types/types';
 import ModalContext from '../context/OtherContexts';
 import contextCart from '../context/contextCart';
@@ -11,9 +11,31 @@ function BasketSummary(props: PropsProds){
     let prodsNumber: number = 0;
     let prodsAmount: number = 0;
     let { setModal } = useContext(ModalContext);
+    let buttonActivePromo = false;
     const { dataCart } = useContext(contextCart);
+    const [coef, setCoef] = useState(1);
+    let promocods:string[] = ['RS','EPM'];
     prodsNumber = dataCart.length;
-    prodsAmount = dataCart.reduce((acum,cur) => acum + cur.objProduct.price,0)
+    const findSum = () =>{
+        prodsAmount = dataCart.reduce((acum,cur) => acum + cur.objProduct.price * coef,0)
+    }
+    findSum();
+    const checkPromocode = (event: React.FormEvent<HTMLInputElement>) => {
+        let currentCode: string = event.currentTarget.value;
+        console.log(coef);
+        let newCoef: number = 1;
+        promocods.forEach((v , i , a) => {
+            console.log(currentCode.indexOf(v));
+            if(currentCode.indexOf(v) >= 0){
+                newCoef -= 0.1;
+            }
+            if(i === a.length - 1){
+                setCoef(newCoef);
+                console.log(coef);
+                findSum();
+            }
+        })
+    }
     const clickButton = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         if(setModal){
@@ -24,9 +46,9 @@ function BasketSummary(props: PropsProds){
         <>
         <div className = "summary-body">
             <h2>Products:<span>{prodsNumber}</span></h2>
-            <h2>Total:<span>€{prodsAmount}</span></h2>
+            <h2>Total:{coef < 1 && <span className='initial-sum'>€{prodsAmount/coef}</span>}<span>€{prodsAmount.toFixed(2)}</span></h2>
             <form>
-                <input type = "text" placeholder="Enter promo code"/>
+                <input type = "text" placeholder="Enter promo code" onInput={(event) => checkPromocode(event)}/>
                 <p>Promo for test: 'RS', 'EPM'</p>
                 <button onClick={clickButton} type = "button">BUY NOW</button>
             </form>
