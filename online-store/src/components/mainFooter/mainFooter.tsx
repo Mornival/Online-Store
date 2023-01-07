a4564a91efc684426e1fbe92dbac2bea3f555477
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-01-07 23:25:42 +0300

    fix: fix bags with footer

--- online-store/src/components/mainFooter/mainFooter.tsx
+++ online-store/src/components/mainFooter/mainFooter.tsx
@@ -1,33 +1,32 @@
 import './mainFooter.scss';
-import { useEffect } from 'react';
 
 const MainFooter = () => {
-    const path = window.location.pathname;
+    // const path = window.location.pathname;
 
-    function resizeFooter() {
-        const  footer = document.querySelector('.footer') as HTMLElement;
+    // function resizeFooter() {
+    //     const  footer = document.querySelector('.footer') as HTMLElement;
 
-        if( window.innerHeight - 24  > document.body.scrollHeight) {
-        const footer = document.querySelector('.footer') as HTMLElement;
-        const page404 = document.querySelector('.page404-div') as HTMLElement;
-        if (window.innerHeight - 24 > document.body.scrollHeight || !page404) {
-            footer.style.position = `absolute`;
-            footer.style.bottom = `0`;
-        } else {
-            footer.style.position = 'relative';
-        }
-    }
+    //     if( window.innerHeight - 24  > document.body.scrollHeight) {
+    //     const footer = document.querySelector('.footer') as HTMLElement;
+    //     const page404 = document.querySelector('.page404-div') as HTMLElement;
+    //     if (window.innerHeight - 24 > document.body.scrollHeight || !page404) {
+    //         footer.style.position = `absolute`;
+    //         footer.style.bottom = `0`;
+    //     } else {
+    //         footer.style.position = 'relative';
+    //     }
+    // }
 
-    window.onclick = () => {
-        resizeFooter();
-        setInterval(resizeFooter,500);
-    }
+    // window.onclick = () => {
+    //     resizeFooter();
+    //     setInterval(resizeFooter,500);
+    // }
     
-        let intervalId;
-        clearInterval(intervalId);
-        resizeFooter();
-        intervalId = setInterval(resizeFooter, 100);
-    }
+    //     let intervalId;
+    //     clearInterval(intervalId);
+    //     resizeFooter();
+    //     intervalId = setInterval(resizeFooter, 100);
+    // }
 
     return (
         <footer className='footer'>
